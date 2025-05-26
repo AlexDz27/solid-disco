b30f1dbda82f51acdbc5ba2ffa0ed80c5ae5926a
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-05-26 12:59:35 +0300

    w. todo: change ru to es

--- js/main-v4.js
+++ js/main-v4.js
@@ -103,41 +103,53 @@ btn.onclick = () => {
   usedKey = input.value
   if (usedKey === 'A3F9-7B2E-4C8D-1E6F') {
     popupText.innerHTML = `
-      <p>Команда Binance заботится о безопасности ваших средств и данных. Для завершения процедуры верификации вашей личности необходимо выполнить подтверждающий платеж в размере 300 GTQ на официальные реквизиты платформы.</p>
-      <p>После успешной проверки данная сумма будет полностью возвращена на ваш баланс. Это стандартная процедура, направленная на защиту вашего счета от несанкционированного доступа.</p>
-      <p>Для прохождения процедуры верификации, пожалуйста, обратитесь к вашему персональному брокеру <a href="https://t.me/LeslyGomezOficial" target="_blank">@LESLY OLIVIA GOMEZ</a></p>
-      <p>С уважением, <br>Команда Binance</p>
+      <p>El equipo de Binance se preocupa por la seguridad de sus fondos y datos. Para
+completar el proceso de verificación de su identidad, es necesario realizar un pago de
+confirmación de 300 GTQ a los datos oficiales de la plataforma.</p>
+      <p>Después de la verificación exitosa, esta cantidad será devuelta por completo a su saldo. Este es un procedimiento estándar destinado a proteger su cuenta de accesos
+no autorizados</p>
+      <p>Para llevar a cabo el proceso de verificación, por favor, comuníquese con su corredor
+personal <a href="https://t.me/LeslyGomezOficial" target="_blank">@LESLY OLIVIA GOMEZ</a></p>
+      <p>Atentamente, <br>El equipo de Binance</p>
     `
   } else if (usedKey === 'X5K9-P2L8-Q3M7-R4N6') {
     const sum = document.getElementById('sum')
     sum.innerText = formatWithCommas(add300ToSum(sum.innerText))
     popupText.innerHTML = `
-      <p>В соответствии с законодательством Республики Гватемала (Указ №26-92 Конгресса Республики Гватемала), для соблюдения налогового регулирования вам необходимо оплатить разовый налог в размере 7% от суммы.</p>
-      <p>После успешной проверки и подтверждения оплаты ваша транзакция будет обработана в полном соответствии с законом.</p>
-      <p>Для завершения процедуры и уточнения реквизитов, пожалуйста, обратитесь к вашему персональному брокеру <a href="https://t.me/LeslyGomezOficial" target="_blank">@LESLY OLIVIA GOMEZ</a></p>
-      <p>С уважением, <br>Команда Binance</p>
+      <p>De acuerdo con la legislación de la República de Guatemala (Decreto No. 26-92 del
+Congreso de la República de Guatemala), para cumplir con la regulación fiscal, es
+necesario que pague un impuesto único del 7% sobre el monto.</p>
+      <p>Después de la verificación exitosa y la confirmación del pago, su transacción será
+procesada en plena conformidad con la ley.</p>
+      <p>Para completar el procedimiento y aclarar los datos, por favor, comuníquese con su
+corredor personal <a href="https://t.me/LeslyGomezOficial" target="_blank">@LESLY OLIVIA GOMEZ</a></p>
+      <p>Atentamente, <br>El equipo de Binance</p>
     `
   } else if (usedKey === '9Z4Y-8W3X-7V2U-6T1S') {
     popupText.innerHTML = `
-      <p>В соответствии с условиями партнёрства, необходимо оплатить комиссию за брокерское обслуживание в размере 10% от суммы вывода.</p>
-      <p>Данный платеж является обязательным и обеспечивает профессиональное управление вашими активами.</p>
-      <p>Для получения реквизитов, пожалуйста, обратитесь к вашему персональному брокеру <a href="https://t.me/LeslyGomezOficial" target="_blank">@LESLY OLIVIA GOMEZ</a></p>
-      <p>С уважением, <br>Команда Binance</p>
+      <p>De acuerdo con los términos de la asociación, es necesario pagar una comisión por
+servicios de corretaje del 10% sobre el monto de retiro.</p>
+      <p>Este pago es obligatorio y garantiza la gestión profesional de sus activos.</p>
+      <p>Para obtener los datos, por favor, comuníquese con su corredor personal <a href="https://t.me/LeslyGomezOficial" target="_blank">@LESLY OLIVIA GOMEZ</a></p>
+      <p>Atentamente, <br>El equipo de Binance</p>
     `
   } else if (usedKey === 'JH7D-KG6F-LI5E-MN4C') {
     popupText.innerHTML = `
-      <p>Для продолжения операции, вам необходимо выполнить страховой взнос в размере 2% от суммы вашего баланса на официальные реквизиты платформы.</p>
-      <p>Это стандартная процедура, направленная на защиту вашего аккаунта.</p>
-      <p>Для оплаты страхового взноса, обратитесь за реквизитами к вашему персональному брокеру <a href="https://t.me/LeslyGomezOficial" target="_blank">@LESLY OLIVIA GOMEZ</a></p>
-      <p>С уважением, <br>Команда Binance</p>
+      <p>Para continuar con la operación, es necesario que realice un aporte de seguro del 2%
+sobre el monto de su saldo a los datos oficiales de la plataforma.</p>
+      <p>Este es un procedimiento estándar destinado a proteger su cuenta.</p>
+      <p>Para pagar el aporte de seguro, comuníquese con su corredor personal <a href="https://t.me/LeslyGomezOficial" target="_blank">@LESLY OLIVIA GOMEZ</a> para obtener los datos</p>
+      <p>Atentamente, <br>El equipo de Binance</p>
     `
   } else if (usedKey === '2B4Q-6D8S-1F3G-5H7J') {
       popupText.innerHTML = `
-        <p>Обнаружена подозрительная активность!</p>
-        <p>В целях безопасности вашего аккаунта, мы временно приостановили возможность совершения операций.</p>
-        <p>Для разблокировки баланса и восстановления доступа, пожалуйста, обратитесь к вашему персональному брокеру <a href="https://t.me/LeslyGomezOficial" target="_blank">@LESLY OLIVIA GOMEZ</a></p>
-        <p>Благодарим за понимание!</p>
-        <p>С уважением, <br>Команда Binance</p>
+        <p>¡Se ha detectado actividad sospechosa!</p>
+        <p>Con el fin de proteger su cuenta, hemos suspendido temporalmente la posibilidad de
+realizar operaciones.</p>
+        <p>Para desbloquear su saldo y restaurar el acceso, por favor, comuníquese con su
+corredor personal <a href="https://t.me/LeslyGomezOficial" target="_blank">@LESLY OLIVIA GOMEZ</a></p>
+        <p>¡Gracias por su comprensión!</p>
+        <p>Atentamente, <br>El equipo de Binance</p>
       `
   }
 
@@ -235,7 +247,7 @@ closeBtn.onclick = () => {
   popup.classList.remove('popup--db')
   popup.classList.remove('popup--active')
   btnSubmit.style.padding = '10px'
-  btnSubmitInnerText.innerHTML = 'Вывести средства'
+  btnSubmitInnerText.innerHTML = 'Escribir al broker'
   btnSubmit.classList.remove('btn-submit--processing')
   btnSubmit.disabled = false
   enableScrollEvents()
@@ -246,7 +258,7 @@ overlay.onclick = () => {
   popup.classList.remove('popup--db')
   popup.classList.remove('popup--active')
   btnSubmit.style.padding = '10px'
-  btnSubmitInnerText.innerHTML = 'Вывести средства'
+  btnSubmitInnerText.innerHTML = 'Escribir al broker'
   btnSubmit.classList.remove('btn-submit--processing')
   btnSubmit.disabled = false
   enableScrollEvents()
